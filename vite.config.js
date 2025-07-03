a0f31bc4b3e5a7cc6b3c05ed117d77d9110c6266
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-07-03 13:09:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- //base: 'https://github.com/Bhuvaneshmaker/employee_listing',
+ base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   
