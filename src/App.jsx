cd3394ac4a2e643f733c42cf6b788f3bd00e2836
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-07-03 11:49:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import HomePage from './pages/HomePage';
 import EmployeeListPage from './pages/EmployeeListPage';
@@ -8,28 +8,29 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-pink-50 to-purple-100">
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/employee" element={<EmployeeListPage />} />
-      </Routes>
-      
-      {/* Toast Notifications */}
-      <ToastContainer
-        position="center"
-        gravity="top"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </div>
+    
+      <div className="min-h-screen bg-gradient-to-br from-pink-50 to-purple-100">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/employee" element={<EmployeeListPage />} />
+        </Routes>
+
+        {/* Toast Notifications */}
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </div>
+   
   );
 }
 
